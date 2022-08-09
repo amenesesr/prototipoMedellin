e5d5fac70b573da630883a7893d079a42e9f4631
Alejandro Meneses Roldan <amenesesr82@gmail.com>
2022-08-09 15:31:34 -0500

    se hizo el cambio agregando las rutas nuevas al app para pruebas y son exitosas

--- Node/app.js
+++ Node/app.js
@@ -3,6 +3,9 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import db from './database/db.js'
 import productosRoutes from './routes/productos.js'
+import clientesRoutes from './routes/clientes.js'
+import usuariosRoutes from './routes/usuarios.js'
+import proveedoresRoutes from './routes/proveedores.js'
 
 dotenv.config({path: './env/.env'})
 
@@ -10,7 +13,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/medellin/', productosRoutes)
+app.use('/medellin/', productosRoutes, clientesRoutes, usuariosRoutes, proveedoresRoutes)
+
 
 try {
     await db.authenticate()
